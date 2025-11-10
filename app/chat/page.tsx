24e487601b8139ccb748b3b2b341dcbb1904ba37
Rayaan Arif <legatum2005@gmail.com>
2025-11-09 18:56:30 -0800

    chat: correct Sidebar import to ../components/Sidebar

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import Sidebar from './components/Sidebar';
+import Sidebar from '../components/Sidebar';
 
 const LS_KEY = 'hr.chat.v1';
 
